cd0c0551ff2965a20ce979b105a89a18b3d4d3b9
Alex Austin <austinalex71@gmail.com>
2026-06-23 19:54:50 -0300

    pasando la carpeta data a public

--- Front-end/FoodStore/src/utils/dataService.ts
+++ Front-end/FoodStore/src/utils/dataService.ts
@@ -4,21 +4,21 @@ import type { IPedido } from "../types/IPedido";
 import type { IUser } from "../types/IUser";
 
 export const obtenerUsuarios = async (): Promise<IUser[]> => {
-  const res = await fetch("/src/data/usuarios.json");
+  const res = await fetch("/data/usuarios.json");
   return res.json();
 };
 
 export const obtenerProductos = async (): Promise<IProducto[]> => {
-  const res = await fetch("/src/data/productos.json");
+  const res = await fetch("/data/productos.json");
   return res.json();
 };
 
 export const obtenerCategorias = async (): Promise<ICategoria[]> => {
-  const res = await fetch("/src/data/categorias.json");
+  const res = await fetch("/data/categorias.json");
   return res.json();
 };
 
 export const obtenerPedidos = async (): Promise<IPedido[]> => {
-  const res = await fetch("/src/data/pedidos.json");
+  const res = await fetch("/data/pedidos.json");
   return res.json();
 };
